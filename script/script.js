fe1e13d6171f413eb5fbd03f1d2b32cb32b54847
reva007kali <le.revaldy@gmail.com>
2024-07-26 02:42:28 +0700

    update

--- script/script.js
+++ script/script.js
@@ -161,3 +161,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// seabsed accordion 
+document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        const isActive = content.style.display === 'block';
+        
+        document.querySelectorAll('.accordion-content').forEach(c => {
+            c.style.display = 'none';
+        });
+        
+        if (!isActive) {
+            content.style.display = 'block';
+        }
+    });
+});
+
